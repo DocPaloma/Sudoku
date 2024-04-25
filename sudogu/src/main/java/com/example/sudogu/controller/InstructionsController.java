51e7dd36104cdd75b1beee3d6d32babc800b9064
DocPaloma <weaqla.xd@gmail.com>
2024-04-25 17:49:30 -0500

    update 2

--- sudogu/src/main/java/com/example/sudogu/controller/InstructionsController.java
+++ sudogu/src/main/java/com/example/sudogu/controller/InstructionsController.java
@@ -1,4 +1,21 @@
 package com.example.sudogu.controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+
 public class InstructionsController {
+    @FXML
+    private ImageView InstructionImage;
+
+    @FXML
+    private Button returnMenuButt;
+
+    @FXML
+    void onHandleButtonAction(ActionEvent event) {
+
+    }
+
 }
