d0575ebdf66a734d88e3ca501798580c1b6fd351
DocPaloma <weaqla.xd@gmail.com>
2024-04-25 18:59:43 -0500

    small test

--- sudogu/src/main/java/com/example/sudogu/controller/InstructionsController.java
+++ sudogu/src/main/java/com/example/sudogu/controller/InstructionsController.java
@@ -1,5 +1,5 @@
 package com.example.sudogu.controller;
-
+import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -7,5 +7,16 @@ import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
 
 public class InstructionsController {
+    public void setInstructionsStage(Stage instructionsStage) {
+        //this.instructionsStage = instructionsStage;
+    }
+
+//    @FXML
+//    private void handleCloseButtonAction() {
+//        // Cerrar la Stage
+//        if (primaryStage != null) {
+//            primaryStage.close();
+//        }
+//    }
 
 }
